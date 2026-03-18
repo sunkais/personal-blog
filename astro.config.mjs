7746e81463e58b12bafa6acd2e9fe2755995da66
sunkais <laolangssss@gmail.com>
2026-03-18 15:46:46 +0800

    feat: 完善数学公式支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-// @ts-check
-
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 });
